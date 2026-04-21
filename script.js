282318cbe683f6514eb00094a029d6a55001dd92
Richard Does Stuff <162537734+richardgrechko@users.noreply.github.com>
2026-04-21 18:29:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,20 +7,24 @@ fetch("./database.txt").then(e=>e.text()).then(d=>{
 		}
 	};
 	const styles = {
-		0: new Info({name: "No ler/wer at all",style:"color:#bc231a"}),
-		2.5: new Info({name: "No ler/wer",style:"color:#ff2324"}),
-		10: new Info({name: "Bottomline ler/wer",style:"color:#ff9020"}),
-		50: new Info({name: "Low ler/wer",style:"color:#ffff20"}),
-		100: new Info({name: "Moderate ler/wer",style:"color:#b0ff20"}),
-		250: new Info({name: "Medium ler/wer",style:"color:#70ff20"}),
-		1000: new Info({name: "High ler/wer",style:"color:#20ff20"}),
-		2500: new Info({name: "Borderline ler/wer",style:"color:#20ff70"}),
-		5000: new Info({name: "Skyline ler/wer",style:"color:#20ffb0"}),
+		[-Infinity]: new Info({name: "Negative $\"ler\" ⁉️",style:"color:#75231a"}),
+		0: new Info({name: "No $\"ler\" at all",style:"color:#bc231a"}),
+		2.5: new Info({name: "No $\"ler\"",style:"color:#ff2324"}),
+		10: new Info({name: "Bottomline $\"ler\"",style:"color:#ff9020"}),
+		50: new Info({name: "Low $\"ler\"",style:"color:#ffff20"}),
+		100: new Info({name: "Moderate $\"ler\"",style:"color:#b0ff20"}),
+		250: new Info({name: "Medium $\"ler\"",style:"color:#70ff20"}),
+		1000: new Info({name: "High $\"ler\"",style:"color:#20ff20"}),
+		2500: new Info({name: "Borderline $\"ler\"",style:"color:#20ff70"}),
+		5000: new Info({name: "Skyline $\"ler\"",style:"color:#20ffb0"}),
 	}
 	const data = {};
 	d = d.split("\n")
 	for (let i of d) {
-		data[i.split(" ")[0]] = Number(i.split(" ")[1])
+		data[i.split(" ")[0]] = {
+			value: Number(i.split(" ")[1]),
+			isFurry: Boolean(i.split(" ")[2])
+		}
 	};
 	for (let i of Object.keys(data)) {
 		let op = document.createElement("option");
@@ -36,9 +40,10 @@ fetch("./database.txt").then(e=>e.text()).then(d=>{
 		}
 	}
 	setInterval(()=>{
-		const val = data[document.getElementById("users").value];
+		const currUser = document.getElementById("users").value
+		const val = data[currUser].value;
 		document.getElementById("output").innerHTML = `${document.getElementById("users").value} has <span style="
 		${styles[round(val)].style}
-		">${styles[round(val)].name}</span> (Ler level: ${val})`;
+		">${styles[round(val)].name.replaceAll(`$"ler"`,(data[currUser].isFurry)?"wer":"ler")}</span> (Ler level: ${val})`;
 	}, 16)
 })
